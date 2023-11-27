36df25cf639c44018f51775abe5a8c229a4e2f74
Danny Holman <dholman@gymli.org>
2023-11-26 18:33:53 -0600

    arch: i386: syscall.h: define some example system calls
    Define a few standard Unix system calls. These may change based on
    future needs.
    
    Signed-off-by: Danny Holman <dholman@gymli.org>

--- arch/i386/include/kernel/syscall.h
+++ arch/i386/include/kernel/syscall.h
@@ -3,12 +3,29 @@
 
 #include <kernel/isr.h>
 
-void halt_catch_fire(struct isr_frame *frame);
-void syscall_dispatch(struct isr_frame *frame);
-void register_syscall(void *handler(struct isr_frame*), int num);
-void sys_stop(struct isr_frame *frame);
-void sys_status(struct isr_frame *frame);
+// Unix standard calls
+#define SYS_FORK        1
+#define SYS_EXIT        2
+#define SYS_WAIT        3
+#define SYS_PIPE        4
+#define SYS_READ        5
+#define SYS_WRITE       6
+#define SYS_KILL        7
+#define SYS_FSTAT       8
+#define SYS_CHDIR       9
+#define SYS_DUP         10
+#define SYS_GETPID      11
+#define SYS_SLEEP       12
+#define SYS_OPEN        13
+#define SYS_MKNOD       14
+#define SYS_UNLINK      15
+#define SYS_LINK        16
+#define SYS_MKDIR       17
+#define SYS_CLOSE       18
 
+#define SYS_REBOOT      88
+
+int handle_syscall(struct isr_frame *frame);
 void dump_reg(struct isr_frame *frame);
 
 #endif
