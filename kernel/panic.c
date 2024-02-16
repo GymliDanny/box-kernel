5ee3595bf405c2c39fc36e15b8c2a3a8d131cf9c
Danny Holman <dholman@gymli.org>
2024-02-16 13:20:23 -0600

    kernel: panic: add a variable that locks other CPUs
    Add a static variable to the panic function that should lock all other
    CPUs.
    
    Signed-off-by: Danny Holman <dholman@gymli.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,8 +1,11 @@
 #include <kernel/panic.h>
 #include <kernel/io.h>
 
+static int panicked = 0;
+
 void panic(const char *str) {
         kprintf("KERNEL PANIC\n");
         kprintf("ERROR: %s\n", str);
+        panicked = 1;
         while (1);
 }
