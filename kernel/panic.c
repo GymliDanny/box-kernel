3851af792ca7b4bb7fc998337c4aec05627cfa8f
Danny Holman <dholman@gymli.org>
2024-06-24 02:09:07 -0500

    kernel: panic: generate a stack frame on panic
    Add a function that generates and prints a stack trace to the screen.
    This function relies on walk_stack() being implemented on the target
    architecture.
    
    Signed-off-by: Danny Holman <dholman@gymli.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -5,8 +5,22 @@
 
 static int panicked = 0;
 
+void walk_stack(uintptr_t *addrs, size_t n);
+
+void stack_trace(void) {
+        kprintf("PRINTING STACK TRACE\n");
+        uintptr_t strace[32];
+        walk_stack(strace, 32);
+        for (int i = 0; i < 32; i++) {
+                if (strace[i] == 0)
+                        break;
+                kprintf("#%d: %x\n", i, strace[i]);
+        }
+}
+
 void panic(const char *str) {
-        kprintf("KERNEL PANIC: %s\n", str);
         panicked = 1;
+        kprintf("KERNEL PANIC: %s\n", str);
+        stack_trace();
         while (1);
 }
